457c9f6d1938b3a517f3f7119f80ccfa81a66c82
Wamundila <51416674+Wamundila@users.noreply.github.com>
2019-06-14 16:28:23 +0200

    added comments

--- variables.js
+++ variables.js
@@ -11,20 +11,24 @@ var youngerAge = olderAge-1;
 console.log("Age before birthday is " + youngerAge);
 
 //PART2
-var currentAge = 24;
-var maxAge = 120;
-var numYears= maxAge-currentAge;
-console.log("I have about "+numYears+" years to live");
+	
+// Calculating number of years to live
+	var currentAge = 24;
+	var maxAge = 120;
+	var numYears= maxAge-currentAge;
+	
+	console.log("I have about "+numYears+" years to live");
 
-var daysInYear=365.25;
-console.log("There are "+daysInYear+" days in a year");
+// Calculating number of days to live
+	var daysInYear=365.25;
+	console.log("There are "+daysInYear+" days in a year");
 
-var numOfDays = daysInYear*numYears;
-console.log("Which means I have "+numOfDays+" days to live");
+	var numOfDays = daysInYear*numYears;
+	console.log("Which means I have "+numOfDays+" days to live");
 
-var applesPerDay = 2;
-console.log("I need "+applesPerDay+" apples per day");
+//Calculating total number of apples needed
+	var applesPerDay = 2;
+	console.log("I need "+applesPerDay+" apples per day");
 
-var totalApples=applesPerDay*numOfDays;
-console.log("Which means I need "+totalApples+" apples");
-//n
+	var totalApples=applesPerDay*numOfDays;
+	console.log("Which means I need "+totalApples+" apples");
