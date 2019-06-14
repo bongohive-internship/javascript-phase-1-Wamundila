7d7cff2de80a3b29ae52477b42d6aebb8c21f566
Wamundila <51416674+Wamundila@users.noreply.github.com>
2019-06-14 15:48:18 +0200

    part1

--- variables.js
+++ variables.js
@@ -1,4 +1,9 @@
 // you solution should be here
 var birthYear = 1995;
-var futureYear;
-console.log("Am working");
+var futureYear=2019;
+// Age after bithday
+var olderAge = futureYear-birthYear;
+console.log("Age after birthday is " + olderAge);
+// Age before bithday
+var youngerAge = olderAge-1;
+console.log("Age before birthday is " + youngerAge);
