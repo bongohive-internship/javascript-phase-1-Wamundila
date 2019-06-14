eff9dce0685be064454bde9c8f985275557000e9
Wamundila <51416674+Wamundila@users.noreply.github.com>
2019-06-14 16:04:59 +0200

    add part 2

--- variables.js
+++ variables.js
@@ -1,3 +1,5 @@
+//PART1
+
 // you solution should be here
 var birthYear = 1995;
 var futureYear=2019;
@@ -7,3 +9,22 @@ console.log("Age after birthday is " + olderAge);
 // Age before bithday
 var youngerAge = olderAge-1;
 console.log("Age before birthday is " + youngerAge);
+
+//PART2
+var currentAge = 24;
+var maxAge = 120;
+var numYears= maxAge-currentAge;
+console.log("I have about "+numYears+" years to live");
+
+var daysInYear=365.25;
+console.log("There are "+daysInYear+" days in a year");
+
+var numOfDays = daysInYear*numYears;
+console.log("Which means I have "+numOfDays+" days to live");
+
+var applesPerDay = 2;
+console.log("I need "+applesPerDay+" apples per day");
+
+var totalApples=applesPerDay*numOfDays;
+console.log("Which means I need "+totalApples+" apples");
+//n
